51ebd5f230e8d70fdc1d96c97ee07869094d24e7
Tony Pujals <tony@subfuzion.com>
2019-02-01 20:10:35 -0800

    add google analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     title: 'App Mesh Guide',
     description: 'Up and Running with AWS App Mesh',
-    base: '/appmesh-guide/'
+    base: '/appmesh-guide/',
+    ga: 'UA-133644962-1'
 }
