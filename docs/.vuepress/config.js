1563c16312c0d94115abd609c8b5b843aa18f03d
Tony Pujals <pujals@amazon.com>
2019-04-02 21:36:03 -0700

    wip

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,55 @@
+let collapsable = true;
+
 module.exports = {
     title: 'App Mesh Guide',
     description: 'Up and Running with AWS App Mesh',
     base: '/appmesh-guide/',
-    ga: 'UA-133644962-1'
-}
+    ga: 'UA-133644962-1',
+    markdown: {
+      lineNumbers: true
+    },
+    plugins: [
+        '@vuepress/active-header-links',
+        '@vuepress/back-to-top',
+        '@vuepress/medium-zoom'
+    ],
+    themeConfig: {
+        sidebar: [
+            {
+                title: "Introduction",
+                collapsable: collapsable,
+                children: [
+                    "/introduction/"
+                ]
+            },
+            {
+                title: "Concepts",
+                collapsable: collapsable,
+                children: [
+                    "/concepts/"
+                ]
+            },
+            {
+                title: "Setup",
+                collapsable: collapsable,
+                children: [
+                    "/setup/"
+                ]
+            },
+            {
+                title: "ECS",
+                collapsable: collapsable,
+                children: [
+                    "/ecs/"
+                ]
+            },
+            {
+                title: "EKS",
+                collapsable: collapsable,
+                children: [
+                    "/eks/"
+                ]
+            }
+        ]
+    }
+};
